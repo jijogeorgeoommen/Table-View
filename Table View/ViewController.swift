80c0ccde44852f94bcacc2001b7d9e6a801d779b
jijogeorgeoommen <jijogeorgeoommen@gmail.com>
2019-05-31 11:14:24 +0530

    Initial Commit

---
+++ Table View/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Table View
+//
+//  Created by JIJO G OOMMEN on 31/05/19.
+//  Copyright © 2019 JIJO G OOMMEN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Table ViewTests/Table_ViewTests.swift
@@ -0,0 +1,34 @@
+//
+//  Table_ViewTests.swift
+//  Table ViewTests
+//
+//  Created by JIJO G OOMMEN on 31/05/19.
+//  Copyright © 2019 JIJO G OOMMEN. All rights reserved.
+//
+
+import XCTest
+@testable import Table_View
+
+class Table_ViewTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
