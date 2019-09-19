fe8f1a46df88ec132d1e85d19e29d0eaedafebb9
jijogeorgeoommen <jijogeorgeoommen@gmail.com>
2019-09-19 08:39:30 +0530

    initial

---
+++ CellOne.swift
@@ -0,0 +1,25 @@
+//
+//  CellOne.swift
+//  Table View
+//
+//  Created by JIJO G OOMMEN on 31/05/19.
+//  Copyright © 2019 JIJO G OOMMEN. All rights reserved.
+//
+
+import UIKit
+
+class CellOne: UITableViewCell {
+
+    @IBOutlet weak var lAbel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Table View/ViewController.swift
+++ Table View/ViewController.swift
@@ -8,8 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate,UITableViewDataSource {
+    
+    var numb = ["123","456","789","10,11,12"]
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+       return numb.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let con =  tAbleOut.dequeueReusableCell(withIdentifier: "Cellid") as! CellOne
+        con.lAbel.text = numb[indexPath.row]
+      //  print(con.lAbel.text)
+        
+        return con
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150
+    }
+    
+    @IBOutlet weak var tAbleOut: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
